c9d15efb6fb653459f62a1bfba6f5e91ff21d28b
cdenm <caltom1up@gmail.com>
2019-05-29 00:11:23 +1000

    Terrain from displacement map
    terrain generates from 1 of 12 predetermined maps. same terrain im mesh mode generated slightly above to create outline effect.
    Cube generated below,
    Added stolen controls that are slightly broken.

--- CG - Assignment/scripts/generateTerrain.js
+++ CG - Assignment/scripts/generateTerrain.js
@@ -1,14 +1,47 @@
 function generateTerrain(){
-  var geometry = new THREE.PlaneBufferGeometry( 30, 30, 10, 10 );
-  geometry.rotateX( - Math.PI / 2 );
-  var vertices = geometry.attributes.position.array;
-  for ( var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3 ) {
-    vertices[ j + 1 ] = (Math.random()*2);
-  }
-  var material = new THREE.MeshBasicMaterial( {color: 0x99cc00, side: THREE.DoubleSide} );
-  material.color = new THREE.Color(0, 255, 0);
-  material.wireframe = true;
-  var plane = new THREE.Mesh( geometry, material );
-  scene.add( plane );
 
+  var geometry = new THREE.PlaneBufferGeometry( 20, 20, 100, 100 );
+  geometry.rotateX( - Math.PI / 2 );
+
+  var loader = new THREE.TextureLoader();
+  var displacement_map = loader.load('textures/' + THREE.Math.randInt(1, 12) + '.jpg');
+
+  var material_ground = new THREE.MeshPhongMaterial({
+      color: 0x99cc00,
+      displacementMap: displacement_map,
+      displacementScale: 5,
+      side: THREE.DoubleSide
+  });
+
+  var material_ground_wire = new THREE.MeshPhongMaterial({
+      color: 0x204c09,
+      displacementMap: displacement_map,
+      displacementScale: 5,
+      side: THREE.DoubleSide
+  });
+  material_ground_wire.wireframe = true;
+
+
+  var mesh = new THREE.Mesh(
+      geometry,
+      material_ground
+    );
+
+
+  var meshWire = new THREE.Mesh(
+      geometry,
+      material_ground_wire
+    );
+  meshWire.position.set(0,0.01,0);
+
+
+  var basegeometry = new THREE.BoxGeometry(20, 10, 20);
+  var material = new THREE.MeshBasicMaterial( {color: 0x443b25} );
+  var base = new THREE.Mesh( basegeometry, material );
+  base.position.set(0,-5.01,0);
+
+
+  scene.add( base );
+  scene.add( mesh );
+  scene.add(meshWire);
 }
