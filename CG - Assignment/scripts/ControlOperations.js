98d1869629b1cd5b7c02edb27b0c1224976224ff
Walter Chan <granck.fate@gmail.com>
2019-05-29 13:30:14 +1000

    Update BuildGui()
    Added Wireframe Support

--- CG - Assignment/scripts/ControlOperations.js
+++ CG - Assignment/scripts/ControlOperations.js
@@ -1,1 +1,16 @@
 //This should handle both keyboard and mouse inputs
+        function BuildGui() {
+            var gui = new dat.GUI();
+
+            var param = {
+                //color: material_tissue.color.getHex(),
+                wireframe : material_ground.wireframe
+            }
+            //gui.addColor(param, 'color').onChange(function(val) {
+            //    material_tissue.color.setHex(val);
+            //});
+            gui.add(param, 'wireframe').onChange(function(bool) {
+                material_ground.wireframe=bool;
+            });
+            gui.open();
+        }
