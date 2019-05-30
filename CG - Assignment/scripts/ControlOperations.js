9a012ed17cb8277468407ef5ac439a6af2fe3a77
YuujiRie <49218709+YuujiRie@users.noreply.github.com>
2019-05-30 12:57:16 +1000

    Add files via upload

--- CG - Assignment/scripts/ControlOperations.js
+++ CG - Assignment/scripts/ControlOperations.js
@@ -1,16 +1,1 @@
 //This should handle both keyboard and mouse inputs
-        function BuildGui() {
-            var gui = new dat.GUI();
-
-            var param = {
-                //color: material_tissue.color.getHex(),
-                wireframe : material_ground.wireframe
-            }
-            //gui.addColor(param, 'color').onChange(function(val) {
-            //    material_tissue.color.setHex(val);
-            //});
-            gui.add(param, 'wireframe').onChange(function(bool) {
-                material_ground.wireframe=bool;
-            });
-            gui.open();
-        }
