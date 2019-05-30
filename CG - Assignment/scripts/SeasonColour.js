99ac7c74b46c122627f480bf2bf2beae3c593773
Walter Chan <granck.fate@gmail.com>
2019-05-30 16:42:31 +1000

    Update SeasonColour.js

--- CG - Assignment/scripts/SeasonColour.js
+++ CG - Assignment/scripts/SeasonColour.js
@@ -21,3 +21,15 @@ let seasons = [
         material_base: 0x443b25,
     }
 ];
+
+function setSeason() {
+    if (updateSeason.Season == "Summer") {
+        material_ground.color.setHex(0x30ec2a);       
+    } else if (updateSeason.Season == "Autumn") {  
+        material_ground.color.setHex(0xffffff);  
+    } else if (updateSeason.Season == "Winter") {   
+        material_ground.color.setHex(0x000000);  
+    } else if (updateSeason.Season == "Spring") {   
+        material_ground.color.setHex(0x30ec2a);  
+    }
+}
