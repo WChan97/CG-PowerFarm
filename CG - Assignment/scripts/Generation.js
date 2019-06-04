e7965ca33a5b150bc37d97fe36d5648cd03da88a
Walter Chan <granck.fate@gmail.com>
2019-06-04 16:05:57 +1000

    Added Regeneration Terrain

--- CG - Assignment/scripts/Generation.js
+++ CG - Assignment/scripts/Generation.js
@@ -1,6 +1,50 @@
 //This should handle terrain and object generation
+function reGen() {
+    geometry = new THREE.PlaneBufferGeometry(20, 20, 100, 100);
+    geometry.rotateX(-Math.PI / 2);
 
-function generation() {
+    loader = new THREE.TextureLoader();
+    displacement_map = loader.load('img/' + THREE.Math.randInt(1, 12) + '.jpg');
+
+    material_ground = new THREE.MeshPhongMaterial({
+        color: 0x99cc00,
+        displacementMap: displacement_map,
+        displacementScale: 5,
+        side: THREE.DoubleSide
+    });
+    material_ground.castShadow = true;
+
+    material_ground_wire = new THREE.MeshPhongMaterial({
+        color: 0x204c09,
+        displacementMap: displacement_map,
+        displacementScale: 5,
+        side: THREE.DoubleSide
+    });
+    material_ground_wire.wireframe = true;
+
+
+    mesh = new THREE.Mesh(
+        geometry,
+        material_ground
+    );
+
+
+    meshWire = new THREE.Mesh(
+        geometry,
+        material_ground_wire
+    );
+    meshWire.position.set(0, 0.01, 0);
+
+
+    basegeometry = new THREE.BoxGeometry(20, 5, 20);
+    material_base = new THREE.MeshBasicMaterial({
+        color: 0x443b25
+    });
+    base = new THREE.Mesh(basegeometry, material_base);
+    base.position.set(0, -2.51, 0);
+}
+
+/*function generation() {
     var cube_geometry = new THREE.BoxGeometry(3, 3, 3);
     var cube_material = new THREE.MeshBasicMaterial();
     cube_material.color = new THREE.Color(255, 0, 0);
@@ -43,4 +87,4 @@ function generation() {
     scene.add(torus);
     scene.add(torusKnot);
     scene.add(cone);
-}
+}*/
